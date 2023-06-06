efa1d074c9268fea981f1e5a1f93084477446609
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-07 00:34:45 +0600

    Primary setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <h1 className="text-9xl font-bold underline text-red-500">Vite + React</h1>
+    </>
+  )
+}
+
+export default App
