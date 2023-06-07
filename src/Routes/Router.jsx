2bedce6ad384d1f9a7dad79d3ace390e1ab2df1c
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-07 14:34:29 +0600

    Favicon added

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main/Main";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/Error/ErrorPage";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+export default router;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            I am fom home
+        </div>
+    );
+};
+
+export default Home;
