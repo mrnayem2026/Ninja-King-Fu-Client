618d6eddba347fd94d7a4f107fff19bc28681a5a
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-08 21:16:24 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCj7E_F130A8QI7WKWquOXOjNZJAef4y_Y",
-  authDomain: "ninja-kung-fu-client-side.firebaseapp.com",
-  projectId: "ninja-kung-fu-client-side",
-  storageBucket: "ninja-kung-fu-client-side.appspot.com",
-  messagingSenderId: "229740475379",
-  appId: "1:229740475379:web:ac507fd8df98309b4a1886"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPIDs
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export default app;
