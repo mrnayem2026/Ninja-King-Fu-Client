f212e0e47c108eac54f12688eaa1ddf489ed3c0d
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-08 09:48:40 +0600

    Footer done

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/shared/Navbar/Navbar";
+import Footer from "../../components/shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
