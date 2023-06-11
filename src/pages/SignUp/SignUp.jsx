43dad88767e2f99c9797d69a6bc33c8aa101643d
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-11 10:49:08 +0600

    Singup from done

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -6,12 +6,15 @@ import registerImg from '../../assets/icons/singUp.png';
 import github from '../../assets/icons/github.png';
 import google from '../../assets/icons/google.png';
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 
 const Signup = () => {
 
     const [confirmPasswordError, setConfirmPasswordError] = useState("")
+    const [confirmPasswordErrorForConfirm, setConfirmPasswordErrorForConfirm] = useState("")
     const [firebaseError, setFrebaseError] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -34,7 +37,7 @@ const Signup = () => {
                         })
                         .catch(error => setFrebaseError(error.message))
 
-                        if (result) {
+                        if (result.user) {
                             reset();
                             Swal.fire({
                                 position: 'top-end',
@@ -81,6 +84,15 @@ const Signup = () => {
             .catch(error => {
                 console.log(error)
             })
+    }
+
+     // Hide password 
+     const handlePasswordHide = () => {
+        setShowPassword(!showPassword)
+    }
+     // Hide password for confirm
+     const handlePasswordHideForConfirm = () => {
+        setConfirmPasswordErrorForConfirm(!confirmPasswordErrorForConfirm)
     }
 
 
@@ -137,7 +149,7 @@ const Signup = () => {
                     {/* password  */}
                     <div className="form-control relative z-0  w-full mb-6 group">
                         <input
-                            type="password"
+                            type={showPassword ? 'text' : 'password'}
                             name="password"
                             id="password"
                             {...register("password", {
@@ -145,7 +157,7 @@ const Signup = () => {
                                 minLength: 6,
                                 pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
                             })}
-                            className={`block py-2.5 px-0  w-full text-sm text-black bg-transparent border-0 border-b-2  appearance-none   focus:outline-none focus:ring-0  peer `}
+                            className={`relative block py-2.5 px-0  w-full text-sm text-black bg-transparent border-0 border-b-2  appearance-none   focus:outline-none focus:ring-0  peer `}
                             placeholder=" "
                             required
                         />
@@ -155,15 +167,33 @@ const Signup = () => {
                         >
                             Password
                         </label>
+                        {
+                            showPassword ?
+                            <span className="absolute top-2 right-0">
+                            <svg onClick={handlePasswordHide}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6  text-black cursor-pointer">
+                                <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                                <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                                <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                            </svg>
+
+                        </span> : 
+                            
+                            <span className="absolute top-2 right-0">
+                                <svg onClick={handlePasswordHide} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-black cursor-pointer">
+                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                                </svg>
+                            </span>
+                        }
                         {errors.password?.type === 'required' && <p className="text-white">Password is required</p>}
                         {errors.password?.type === 'minLength' && <p className="text-white">Password must be 6 characters</p>}
                         {errors.password?.type === 'pattern' && <p className="text-white">Password must have one Capital Letter and one Special Character.</p>}
 
                     </div>
-                    {/* password  */}
+                    {/*  Confirm Password  */}
                     <div className="form-control relative z-0  w-full mb-6 group">
                         <input
-                            type="password"
+                             type={confirmPasswordErrorForConfirm ? 'text' : 'password'}
                             name="password"
                             id="ConfirmPassword"
                             {...register("confirmPassword", { required: true, })}
@@ -177,6 +207,24 @@ const Signup = () => {
                         >
                             Confirm Password
                         </label>
+                        {
+                            confirmPasswordErrorForConfirm ?
+                            <span className="absolute top-2 right-0">
+                            <svg onClick={handlePasswordHideForConfirm}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6  text-black cursor-pointer">
+                                <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                                <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                                <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                            </svg>
+
+                        </span> : 
+                            
+                            <span className="absolute top-2 right-0">
+                                <svg onClick={handlePasswordHideForConfirm} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-black cursor-pointer">
+                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                                </svg>
+                            </span>
+                        }
                         {errors.confirmPassword?.type === 'required' && <p className="text-white">Password is required</p>}
                     </div>
 
