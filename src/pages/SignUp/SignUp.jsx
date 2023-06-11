e5af28d722d51063de60a7357dad6f073aa613c5
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-11 10:37:06 +0600

    SIngup method on fire

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -5,62 +5,84 @@ import useAuth from "../../customeHocks/useAuth";
 import registerImg from '../../assets/icons/singUp.png';
 import github from '../../assets/icons/github.png';
 import google from '../../assets/icons/google.png';
+import { useState } from "react";
 
 
 const Signup = () => {
 
-
+    const [confirmPasswordError, setConfirmPasswordError] = useState("")
+    const [firebaseError, setFrebaseError] = useState("")
 
     const navigate = useNavigate();
     const location = useLocation();
 
-    const { createUser } = useAuth();
+    const { createUser, updateUserProfile } = useAuth();
 
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-
-
-
-    const updateUserData = (loggedUser, name, photoUrl) => {
-        updateProfile(loggedUser, {
-            displayName: name,
-            photoURL: photoUrl
-        }).then(() => {
-            console.log('user name updated')
-        })
-            .catch(error => {
-                setError(error.message);
-            })
-
-
-    }
-        // Google signin 
-
-        const handleGoogleSignIn = () => {
-            signInWithGoogle()
+    const onSubmit = data => {
+        console.log(data)
+        if (data.password == data.confirmPassword) {
+            console.log("Success");
+            createUser(data.email, data.password)
                 .then(result => {
                     const loggedUser = result.user;
                     console.log(loggedUser);
-                    navigate("/");
+                    updateUserProfile(data.name, data.photoURL)
+                        .then(()=> {
+                            setFrebaseError("")
+                        })
+                        .catch(error => setFrebaseError(error.message))
+
+                        if (result) {
+                            reset();
+                            Swal.fire({
+                                position: 'top-end',
+                                icon: 'success',
+                                title: 'User created successfully.',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            navigate('/');
+                        }
                 })
-                .catch(error => {
-                    console.log(error)
-                })
+                .catch(error => setFrebaseError(error.message))
+
+            setConfirmPasswordError("")
+        } else {
+            setConfirmPasswordError("Password and Confirm Password don't match");
         }
-    
-        // GitHub SignIn 
-        const handleGitHubSignIn = () => {
-            singInWithGithub()
-                .then(result => {
-                    const loggedUser = result.user;
-                    navigate("/");
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
-    
+    };
+
+
+
+
+    // Google signin 
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate("/");
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    // GitHub SignIn 
+    const handleGitHubSignIn = () => {
+        singInWithGithub()
+            .then(result => {
+                const loggedUser = result.user;
+                navigate("/");
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
 
     return (
         <div className="mt-20 container mx-auto  flex gap-56" >
@@ -69,6 +91,10 @@ const Signup = () => {
             </div>
             {/* Register from strat */}
             <div className="mt-24 bg-gradient-to-tr from-fuchsia-400 to-purple-600 w-2/6 pt-20 p-10 mb-10 ">
+                <div className="pb-7 text-white">
+                    <p>{confirmPasswordError}</p>
+                    <p>{firebaseError}</p>
+                </div>
                 <form onSubmit={handleSubmit(onSubmit)} className="">
 
                     {/* name  */}
@@ -86,7 +112,7 @@ const Signup = () => {
                         >
                             Name
                         </label>
-                        {errors.name && <span className="text-red-600">Name is required</span>}
+                        {errors.name && <span className="text-white">Name is required</span>}
                     </div>
 
                     {/* email  */}
@@ -105,7 +131,7 @@ const Signup = () => {
                         >
                             Email address
                         </label>
-
+                        {errors.email && <span className="text-white">Email is required</span>}
                     </div>
 
                     {/* password  */}
@@ -129,9 +155,9 @@ const Signup = () => {
                         >
                             Password
                         </label>
-                        {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
-                        {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
-                        {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Capital Letter and one Special Character.</p>}
+                        {errors.password?.type === 'required' && <p className="text-white">Password is required</p>}
+                        {errors.password?.type === 'minLength' && <p className="text-white">Password must be 6 characters</p>}
+                        {errors.password?.type === 'pattern' && <p className="text-white">Password must have one Capital Letter and one Special Character.</p>}
 
                     </div>
                     {/* password  */}
@@ -140,6 +166,7 @@ const Signup = () => {
                             type="password"
                             name="password"
                             id="ConfirmPassword"
+                            {...register("confirmPassword", { required: true, })}
                             className={`block py-2.5 px-0  w-full text-sm text-black bg-transparent border-0 border-b-2  appearance-none   focus:outline-none focus:ring-0  peer `}
                             placeholder=" "
 
@@ -150,7 +177,7 @@ const Signup = () => {
                         >
                             Confirm Password
                         </label>
-
+                        {errors.confirmPassword?.type === 'required' && <p className="text-white">Password is required</p>}
                     </div>
 
                     {/* photoURL  */}
@@ -168,7 +195,7 @@ const Signup = () => {
                         >
                             Image URL
                         </label>
-                        {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                        {errors.photoURL && <span className="text-white">Photo URL is required</span>}
                     </div>
 
                     <div className='flex flex-col justify-center items-center gap-6'>
