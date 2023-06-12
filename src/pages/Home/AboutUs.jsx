691ecf4aa4b0e6f3d01a35e513a69c439c794d8a
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-12 10:07:29 +0600

    Extra section done

---
+++ src/pages/Home/AboutUs.jsx
@@ -0,0 +1,21 @@
+import aboutUsImg from '../../assets/img/about.png'
+import aboutUsImg1 from '../../assets/img/about1.jpg'
+import aboutUsImg2 from '../../assets/img/about2.jpg'
+const AboutUs = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row gap-20">
+                <img src={aboutUsImg} className="max-w-sm rounded-lg "/>
+                <div>
+                    <h1 className='text-4xl lg:text-7xl font-Montserrat font-semibold text-[#F87272]'>About Us</h1>
+                    <p className='font-Kanit font-extralight text-2xl'>Explore Ninja Kung Fu with us</p>
+                    <p className="py-6 w-3/4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima nihil corrupti cupiditate. Porro perspiciatis corrupti doloribus, odit impedit hic dolorem nisi necessitatibus blanditiis omnis a aspernatur rerum nam sit minus voluptatum eveniet quaerat excepturi totam ratione distinctio fugit dolor. Expedita?</p>
+                    <p className="pb-6  w-3/4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima nihil corrupti cupiditate. Porro perspiciatis corrupti doloribus, odit impedit hic dolorem nisi necessitatibus blanditiis omnis a aspernatur rerum nam sit minus voluptatum eveniet quaerat excepturi totam ratione distinctio fugit dolor. Expedita?</p>
+                    <button className="btn btn-outline hover:bg-[#F87272] font-semibold">Explore More</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from '../../components/shared/Banner/Banner';
-import BannerTitle from '../../components/shared/BannerTitle/BannerTitle';
+import AboutUs from './AboutUs';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AboutUs></AboutUs>
         </div>
     );
 };
