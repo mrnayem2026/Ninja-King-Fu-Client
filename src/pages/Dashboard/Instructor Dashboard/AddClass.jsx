fb16fbf7afd7571bcf1f0b673389df4925eccfaa
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-14 00:44:20 +0600

    Add a class done

--- src/pages/Dashboard/Instructor Dashboard/AddClass.jsx
+++ src/pages/Dashboard/Instructor Dashboard/AddClass.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../../customeHocks/useAuth';
+import useAxiosSecure from '../../../customeHocks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const AddClass = () => {
 
 
     const { register, handleSubmit, reset } = useForm();
-    const {user} = useAuth();
-console.log(user?.displayName);
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
+
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+
     const onSubmit = data => {
-        console.log(data);
+
+        const formData = new FormData();
+        formData.append('image', data.image[0])
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    const { name, price, email, className,availableSeats } = data;
+                    const newClass = { name, price: parseFloat(price), email, className,availableSeats, image: imgURL }
+                    console.log(newClass)
+                    axiosSecure.post('/class', newClass)
+                        .then(data => {
+                            console.log('after posting new menu item', data.data)
+                            if (data.data.insertedId) {
+                                reset();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'New Class added successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
+                }
+            })
 
     };
 
@@ -31,7 +68,7 @@ console.log(user?.displayName);
                         <label className="label">
                             <span className="label-text font-semibold">Instructor email*</span>
                         </label>
-                        <input type="text" defaultValue={user?.email} readOnly placeholder="Instructor email"
+                        <input type="email" defaultValue={user?.email} readOnly placeholder="Instructor email"
                             {...register("email", { required: true, maxLength: 120 })}
                             className="input input-bordered w-full text-white" />
                     </div>
