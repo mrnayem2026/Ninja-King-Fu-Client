5bb603696bfb5c5c7d3a710f65517196df232aea
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-13 20:46:22 +0600

    Manage user setup done

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import './index.css'
 import router from './Routes/Router'
 import { RouterProvider } from 'react-router-dom'
 import AuthProviders from './Providers/AuthProviders/AuthProviders'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProviders>
-      <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
     </AuthProviders>
   </React.StrictMode>,
 )
--- src/pages/Dashboard/Admin Dashboard/ManageUsers.jsx
+++ src/pages/Dashboard/Admin Dashboard/ManageUsers.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import useAxiosSecure from '../../../customeHocks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const ManageUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const { data: users = [], refetch, isLoading } = useQuery(['users'], async () => {
+        const res = await axiosSecure.get('/users')
+        return res.data;
+    })
+
+    if (isLoading) {
+        return <span>Loading...</span>
+      }
+
+
     return (
-        <div>
-            I am from Manage Users
+        <div className='text-black w-full p-10'>
+            <div  className='w-52 h-28 bg-[#F5F5F7] flex items-center justify-center rounded-xl gap-3'>
+                <h1> <span className='text-3xl font-Montserrat font-semibold'>Total <br />Users </span> </h1>
+                <span className='text-7xl font-Montserrat font-semibold'>{users.length}</span>
+            </div>
+            {
+                console.log(users)
+            }
+            <div className="overflow-x-auto ">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Make Instructor</th>
+                            <th>Make Admin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+
+                        {
+                            users.map((user, index) => 
+                                <tr key={user._id}>
+                                    <th>{index+1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>Blue</td>
+                                    <td>Blue</td>
+                                </tr>
+                            )
+
+
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
