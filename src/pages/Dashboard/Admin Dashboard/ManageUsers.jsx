3f734d19811a0a0412a2670b322d0be8da381af1
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-13 22:28:36 +0600

    handleMakeInstructordone

--- src/pages/Dashboard/Admin Dashboard/ManageUsers.jsx
+++ src/pages/Dashboard/Admin Dashboard/ManageUsers.jsx
@@ -13,45 +13,67 @@ const ManageUsers = () => {
     })
 
     const [disabledButtons, setDisabledButtons] = useState([]);
+    const [instructorDisabledButtons, setinstructorDisabledButtons] = useState([]);
 
     if (isLoading) {
         return <div className='flex justify-center items-center h-screen'>
-            <HashLoader color="#A6ADBA"  />
+            <HashLoader color="#A6ADBA" />
         </div>
-      }
-
-      const handleMakeAdmin = (user,index) =>{
-      
-        fetch(`http://localhost:5000/users/admin/${user._id}`, {
-            method: 'PATCH'
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-          
-            if(data.modifiedCount){
-                refetch();
-                
-                const updatedDisabledButtons = disabledButtons.slice();
-                updatedDisabledButtons[index] = true;
-        
-                setDisabledButtons(updatedDisabledButtons);
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: `${user.name} is an Admin Now!`,
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-        })
     }
+
+    const handleMakeAdmin = (user, index) => {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(response => {
+                if (response.data.modifiedCount) {
+                    refetch();
+                    const updatedDisabledButtons = disabledButtons.slice();
+                    updatedDisabledButtons[index] = true;
+
+                    setDisabledButtons(updatedDisabledButtons);
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is an Admin Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(error => {
+                // Handle any error that occurs during the request
+                console.error(error);
+            });
+    }
+    const handleMakeInstructor = (user, index) => {
+        axiosSecure.patch(`/users/instructor/${user._id}`)
+            .then(response => {
+                if (response.data.modifiedCount) {
+                    refetch();
+                    const updatedDisabledButtons = instructorDisabledButtons.slice();
+                    updatedDisabledButtons[index] = true;
+
+                    setinstructorDisabledButtons(updatedDisabledButtons);
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is a Instructor Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(error => {
+                // Handle any error that occurs during the request
+                console.error(error);
+            });
+    }
+
 
 
 
     return (
         <div className='text-black w-full p-10'>
-            <div  className='w-52 h-28 bg-[#F5F5F7] flex items-center justify-center rounded-xl gap-3'>
+            <div className='w-52 h-28 bg-[#F5F5F7] flex items-center justify-center rounded-xl gap-3'>
                 <h1> <span className='text-3xl font-Montserrat font-semibold'>Total <br />Users </span> </h1>
                 <span className='text-7xl font-Montserrat font-semibold'>{users.length}</span>
             </div>
@@ -74,13 +96,13 @@ const ManageUsers = () => {
                         {/* row 1 */}
 
                         {
-                            users.map((user, index) => 
+                            users.map((user, index) =>
                                 <tr key={user._id}>
-                                    <th>{index+1}</th>
+                                    <th>{index + 1}</th>
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
-                                    <td><button  className="btn btn-outline text-lg font-Montserrat font-extrabold">Instructor</button></td>
-                                    <td><button onClick={() => handleMakeAdmin(user,index)}     disabled={disabledButtons[index]}  className="btn btn-outline text-lg font-Montserrat font-extrabold">admin</button></td>
+                                    <td><button onClick={() => handleMakeInstructor(user, index)} disabled={instructorDisabledButtons[index]} className="btn btn-outline text-lg font-Montserrat font-extrabold">Instructor</button></td>
+                                    <td><button onClick={() => handleMakeAdmin(user, index)} disabled={disabledButtons[index]} className="btn btn-outline text-lg font-Montserrat font-extrabold">admin</button></td>
                                 </tr>
                             )
 
