c1f71adfb6159eac106530896298fca94b667972
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-14 09:30:39 +0600

    Class update done

--- src/pages/Dashboard/Instructor Dashboard/MyClasses.jsx
+++ src/pages/Dashboard/Instructor Dashboard/MyClasses.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import useAxiosSecure from '../../../customeHocks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../customeHocks/useAuth';
 import { HashLoader } from 'react-spinners';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const MyClasses = () => {
+    const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+    const [classId, setClassId] = useState(null);
     const { register, handleSubmit, reset } = useForm();
     const [axiosSecure] = useAxiosSecure();
     const { user } = useAuth();
@@ -21,9 +26,45 @@ const MyClasses = () => {
         </div>
     }
 
-    const onSubmit = (data, event) => {
+    const onSubmit = async (data, event) => {
         event.preventDefault();
-        console.log(data);
+        try {
+
+            const formData = new FormData();
+            formData.append('image', data.image[0])
+            fetch(img_hosting_url, {
+                method: 'POST',
+                body: formData
+            })
+                .then(res => res.json())
+                .then(imgResponse => {
+                    if (imgResponse.success) {
+                        const imgURL = imgResponse.data.display_url;
+                        const { name, price, email, className, availableSeats } = data;
+                        const upDateClass = { name, price: parseFloat(price), email, className, availableSeats, image: imgURL }
+                        console.log(upDateClass)
+                        axiosSecure.put(`/class/${classId}`, upDateClass)
+                            .then(data => {
+                                if (data.data.matchedCount) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'New Class added successfully',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    })
+                                }
+                                refetch();
+                                console.log(data)
+                            })
+                    }
+                })
+            window.my_modal_4.close();
+            
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -45,7 +86,10 @@ const MyClasses = () => {
                                 <p>Feedback</p> <span>---------</span>
                                 <p>No Feedback</p>
                                 <div className="card-actions ">
-                                    <button className="btn btn-primary w-full" onClick={() => window.my_modal_4.showModal()}>Update Class</button>
+                                    <button className="btn btn-primary w-full" onClick={() => {
+                                        setClassId(priod._id);
+                                        window.my_modal_4.showModal();
+                                    }}>Update Class</button>
                                 </div>
                             </div>
                         </div>
@@ -110,7 +154,7 @@ const MyClasses = () => {
                         <input type="file" {...register("image", { required: true })} className="opacity-0 absolute  file-input file-input-bordered w-full " />
                     </div>
                     <input className="btn btn-sm mt-4  w-full text-2xl font-Montserrat font-medium" type="submit" value="UpDate Class" />
-                   
+
                     <div className="modal-action">
                         {/* if there is a button, it will close the modal */}
                         <button className="btn" onClick={(e) => { e.preventDefault(); window.my_modal_4.close(); }}>Close</button>
